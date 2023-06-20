bbb2867110d810285baf374eae7d0891597c062d
cymarkSynergy <ogbodouchenna1425@gmail.com>
2023-06-20 22:55:18 +0100

    developed OTP

--- src/app/components/auth/singin/singin.component.ts
+++ src/app/components/auth/singin/singin.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import { Component, VERSION } from '@angular/core';
+import { NgxOtpInputConfig } from 'ngx-otp-input';
 
 @Component({
   selector: 'app-singin',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./singin.component.scss']
 })
 export class SinginComponent {
+
+  otpInputConfig: NgxOtpInputConfig = {
+    otpLength: 6,
+    autofocus: true,
+    classList: {
+      inputBox: 'my-super-box-class',
+      input: 'my-super-class',
+      inputFilled: 'my-super-filled-class',
+      inputDisabled: 'my-super-disable-class',
+      inputSuccess: 'my-super-success-class',
+      inputError: 'my-super-error-class',
+    },
+  };
+
+  handeOtpChange(value: string[]): void {
+    console.log(value);
+  }
+
+  handleFillEvent(value: string): void {
+    console.log(value);
+  }
+
+
+  showModal = false;
+  toggleModal(){
+    this.showModal = !this.showModal;
+  }
 
 }
