8a55e34193973a0025f5591b0b7860e0c4249ea9
user <user@MacBook-Pro.local>
2024-01-16 20:31:03 -0800

    refactored the routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'trevautyApp';
+
+// these route are allowed to have the header and footer
+  allowedRoutes: string[] = [
+    '/home',
+    '/nfc',
+    '/about',
+    '/receipt',
+    '/card',
+    '/pos',
+    '/inventory',
+    '/signup',
+    '/login',
+    '/password-reset'
+  ];
+
+  showNavbar: boolean = false;
+  showFooter: boolean = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const currentRoute = event.url;
+        this.showNavbar = this.allowedRoutes.includes(currentRoute);
+        //  event.url.includes('/login') || event.url.includes('/signup');
+        this.showFooter = this.showNavbar;
+      }
+    });
+  }
 }
+
