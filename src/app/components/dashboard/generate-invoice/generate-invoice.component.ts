8518c70b2c268420f274526d2e90e08b10dc3c0c
user <user@MacBook-Pro.local>
2024-04-17 22:22:31 -0700

    added invoice

--- src/app/components/dashboard/generate-invoice/generate-invoice.component.ts
+++ src/app/components/dashboard/generate-invoice/generate-invoice.component.ts
@@ -5,6 +5,15 @@ import { Router } from '@angular/router';
 import { NgToastService } from 'ng-angular-popup';
 import { TerminalService } from 'src/app/services/terminal.service';
 
+
+interface RowData {
+  description: string;
+  unitPrice: number;
+  quantity: number;
+  tax: number;
+  total: number;
+}
+
 @Component({
   selector: 'app-generate-invoice',
   templateUrl: './generate-invoice.component.html',
@@ -12,16 +21,55 @@ import { TerminalService } from 'src/app/services/terminal.service';
 })
 export class GenerateInvoiceComponent {
 
+  invoice = {
+    name:"",
+    phoneNumber:"",
+    emailAddress:"",
+    address:"",
+    rows : [
+      this.createRowObject('Item 1', 10, 2, 3)
+    ],
+    invoiceDate:"",
+    invoiceNumber:"",
+    paymentDueDate:""
+
+  }
+
+  createRowObject(description: string, unitPrice: number, quantity:number, tax: number) {
+    return {
+      description: description,
+      unitPrice: unitPrice,
+      quantity: quantity,
+      tax: tax,
+      total: unitPrice * quantity
+    };
+  }
+
+  getSubTotal(): number{
+    let subTotal = 0;
+      this.invoice.rows.map((item : any)=>{
+      subTotal = subTotal + item.total;
+  });
+  return subTotal;
+}
+
+getTaxSubTotal(): number{
+  let subTotal = 0;
+    this.invoice.rows.map((item : any)=>{
+    subTotal = subTotal + item.tax;
+});
+return subTotal;
+}
+
+getTotal(): number{
+  return this.getSubTotal() - this.getTaxSubTotal();
+}
+
 
   goToManageCard(){
     this.router.navigate(["dashboard/cards"])
 
   }
-
-
-
-
-
 
   showModal = false;
   formSubmitted: boolean = false;
@@ -42,15 +90,20 @@ export class GenerateInvoiceComponent {
 
   ) {
     this.myform = new FormGroup({
-      first6digits: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{11}$')]),
-      last4digits: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{11}$')]),
-      issuer: new FormControl('', [Validators.required]),
-      cardType: new FormControl('', [Validators.required]),
-      expiry: new FormControl('', [Validators.required]),
+      name: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{11}$')]),
+      phoneNumber: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{11}$')]),
+      emailAddress: new FormControl('', [Validators.required]),
+      address: new FormControl('', [Validators.required]),
+      invoiceDate: new FormControl('', [Validators.required]),
+      invoiceNumber: new FormControl('', [Validators.required]),
+      paymentDueDate: new FormControl('', [Validators.required]),
+
     });
   }
 
-  get formData() { return this.myform?.controls; };
+  get formData() { 
+    return this.myform?.controls;
+   };
 
   validateForm() {
 
@@ -80,60 +133,63 @@ export class GenerateInvoiceComponent {
     });
   }
 
-  onSubmit(user: any): void {
-    console.log("this.myform.value");
+  onSubmit(): void {
+    console.log(this.invoice);
+    this.toggleShowInvoicePdf();
+    // console.log("this.myform.value");
 
-    console.log(this.myform?.value);
-    this.showSuccessResponse(this.message, "Sign Up", 3000);
-    // this.showError(this.message + "heeee");
-    this.toast.success({ detail: "hello", summary: "message", duration: 5000 });
+    // console.log(this.myform?.value);
+    // this.showSuccessResponse(this.message, "Sign Up", 3000);
+    // this.toast.success({ detail: "hello", summary: "message", duration: 5000 });
 
-    if (true) {
 
-      this.cardservice.addCardPan(this.myform.value).subscribe({
-        next: (response: any) => {
-          console.log("response =>>>>", response);
-          // this.resetFormInputs();
-          this.myform.markAsPristine(); // Reset form state
-          this.myform.markAsUntouched(); // Reset form state
-          if (response?.message) {
-            console.log("step 1")
-            this.message = response?.message;
-            this.showSuccessResponse(this.message, "Sign Up", 3000);
-            alert(this.message);
+    //   this.cardservice.addCardPan(this.myform.value).subscribe({
+    //     next: (response: any) => {
+    //       console.log("response =>>>>", response);
+    //       this.myform.markAsPristine(); 
+    //       this.myform.markAsUntouched(); 
+    //       if (response?.message) {
+    //         console.log("step 1")
+    //         this.message = response?.message;
+    //         this.showSuccessResponse(this.message, "Sign Up", 3000);
+    //         alert(this.message);
 
-          } else {
-            console.log("step 2")
+    //       } else {
+    //         console.log("step 2")
 
-            // this.message = response?.debugMessage;
-            this.showErrorResponse(response?.debugMessage, "Sign up", 3000);
-            alert(response?.debugMessage);
+    //         this.showErrorResponse(response?.debugMessage, "Sign up", 3000);
+    //         alert(response?.debugMessage);
 
-          }
+    //       }
 
-          // this.router.navigate(['login']);
-        },
-        error: (error: any) => {
-          console.log("sign up failed", error);
-          // Handle validation errors
-          if (error.status === 400 && error.error && error.error.errors) {
-            let errRes = error?.response;
-            let errReason = error?.debugMessage;
-            this.showErrorResponse(errRes + errReason, "Sign Up", 3000);
-            // You can access the validation errors from the error object
-            const validationErrors = error.error.errors;
-            // Display the validation errors or handle them accordingly
-          }
-        }
-      });
-    } else {
-      console.log(user);
-      // this.validateForm();
-    }
+    //     },
+    //     error: (error: any) => {
+    //       console.log("sign up failed", error);
+    //       if (error.status === 400 && error.error && error.error.errors) {
+    //         let errRes = error?.response;
+    //         let errReason = error?.debugMessage;
+    //         this.showErrorResponse(errRes + errReason, "Sign Up", 3000);
+    //         const validationErrors = error.error.errors;
+    //       }
+    //     }
+    //   });
   }
 
   toggleShowInvoicePdf(){
     this.showInvoicePdf = !this.showInvoicePdf;
+  }
+
+
+
+  addRow() {
+    console.log("add row");
+    this.invoice.rows.push({ description: '', unitPrice: 0, quantity: 0, tax: 0, total: 0 });
+
+    console.log(this.invoice.rows);
+  }
+
+  removeRow(index: number) {
+    this.invoice.rows.splice(index, 1);
   }
 
 
